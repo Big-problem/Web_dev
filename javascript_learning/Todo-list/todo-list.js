3b9591b1b759d5f322dea113cb7a51977e417528
Big-problem <117372204+Big-problem@users.noreply.github.com>
2023-08-12 02:14:20 +0800

    Keeping learning JavaScript
    Learn more about function
    learn setTimeout and forEach

--- javascript_learning/Todo-list/todo-list.js
+++ javascript_learning/Todo-list/todo-list.js
@@ -44,6 +44,7 @@ function addTodo3() {
 function showTodoList() {
     let html = '';
     let todoHTML = '';
+    /*
     for(let i = 0; i < todoList.length; i++){
         // const name = todoList[i].name;
         // const date = todoList[i].date;
@@ -55,6 +56,17 @@ function showTodoList() {
         `;  // Generating HTML
         todoHTML += html;      
     }
+    */
+   // Use Array.forEach to loop an array
+    todoList.forEach(function(todoObject, i) {
+        const {name, date} = todoObject;
+        html = `
+        <div>${name}</div>
+        <div>${date}</div>
+        <button class="delete-btn" onclick="todoList.splice(${i}, 1); showTodoList()">Delete</button>
+        `;  // Generating HTML
+        todoHTML += html;     
+    });
     document.querySelector('.js-todo-list3').innerHTML = todoHTML;
 }
 function addTodoEnter3(event) {
